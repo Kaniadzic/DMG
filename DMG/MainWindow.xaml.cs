3c92b9c15ba5a78851341855bb43d22d2ece97d8
Kaniadzic <szymon.kania@edu.uekat.pl>
2023-04-09 14:01:52 +0200

    Code: weapon selection

--- DMG/MainWindow.xaml.cs
+++ DMG/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
 using DMG.Entities;
+using DMG.Enums;
 using DMG.Logic;
 
 namespace DMG
@@ -19,8 +20,46 @@ namespace DMG
             BoardLogic boardLogic = new BoardLogic();
             Weapon weaponLogic = new Weapon();
 
-            List<Weapon> weapons = weaponLogic.initializeWeapons();
             Enemy[,] board = boardLogic.generateBoard(generator.generateBoardEnemies());
+        }
+        private void Button_Weapon_Gun_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Karabin", DamageValues.weak, PenetrationValues.weak, WeaponType.hitscan, 100);
+        }
+
+        private void Button_Weapon_Cannon_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Działko", DamageValues.medium, PenetrationValues.medium, WeaponType.hitscan, 100);
+        }
+
+        private void Button_Weapon_Flechettes_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Fleszetki", DamageValues.weak, PenetrationValues.weak, WeaponType.hitscan, 75);
+        }
+
+        private void Button_Weapon_Rocket_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Rakieta", DamageValues.medium, PenetrationValues.strong, WeaponType.hitscan, 50);
+        }
+
+        private void Button_Weapon_ExplosiveBomb_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Bomba burząca", DamageValues.strong, PenetrationValues.strong, WeaponType.hitscan, 100);
+        }
+
+        private void Button_Weapon_ShardBomb_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Bomba tocząca", DamageValues.strong, PenetrationValues.strong, WeaponType.hitscan, 100);
+        }
+
+        private void Button_Weapon_FlameBomb_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Bomba odłamkowa", DamageValues.weak, PenetrationValues.weak, WeaponType.hitscan, 100);
+        }
+
+        private void Button_Weapon_RollingBomb_Click(object sender, RoutedEventArgs e)
+        {
+            this.selectedWeapon = new Weapon("Bomba zapalająca", DamageValues.medium, PenetrationValues.weak, WeaponType.hitscan, 100);
         }
     }
 }
